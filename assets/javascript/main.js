cf555ed4392061d7b85852e9c2380f9e5517988b
Dan Brunner <daninbham@gmail.com>
2025-09-16 18:04:59 -0500

    #13: added buttons for scroll down and up

---
+++ assets/javascript/main.js
@@ -0,0 +1,29 @@
+const sections = document.querySelectorAll("section");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  const scrollLimit = 300;
+  const arrowTop = document.getElementById("arrowTop");
+  const arrowDown = document.getElementById("arrowDown");
+  if (
+    document.body.scrollTop > scrollLimit ||
+    document.documentElement.scrollTop > scrollLimit
+  ) {
+    arrowTop.classList.add("show");
+    arrowDown.classList.remove("show");
+  } else {
+    arrowTop.classList.remove("show");
+    arrowDown.classList.add("show");
+  }
+}
+
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
+function scrollDown() {
+  document.getElementById("about").scrollIntoView();
+}
